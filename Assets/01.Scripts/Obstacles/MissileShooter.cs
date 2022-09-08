7740f6267e475f37f76d534690a12606e8fe7681
Oniboogie <qwes348@naver.com>
2022-09-09 01:57:09 +0900

    미사일 발사 경고 구현 / 미사일 연속발사 시스템 구현

--- Assets/01.Scripts/Obstacles/MissileShooter.cs
+++ Assets/01.Scripts/Obstacles/MissileShooter.cs
@@ -2,13 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
+using DG.Tweening;
 
 public class MissileShooter : MonoBehaviour
 {
-    public List<Transform> missileFirePosList;
+    public List<Transform> missileAlertList;
 
     [Title("ÇÁ¸®ÆÕ")]
     public Poolable missilePrefab;
+
+    private void Start()
+    {
+        Init();
+    }
 
     private void Update()
     {
@@ -16,15 +22,64 @@ public class MissileShooter : MonoBehaviour
             FireMissile();
     }
 
-    public void FireMissile()
+    public void Init()
     {
+        Vector3 alertPos = Camera.main.ViewportToWorldPoint(new Vector3(0.96f, 0.5f));
+        alertPos.z = 0f;
+
+        foreach(var alert in missileAlertList)
+        {
+            alert.position = alertPos;
+            //alert.GetComponent<SpriteRenderer>().enabled = false;
+            alert.gameObject.SetActive(false);
+        }
+    }
+
+    public void FireMissile(int count = 1)
+    {
+        StartCoroutine(ReadyFireMissileCor(count));
+    }
+
+    IEnumerator ReadyFireMissileCor(int fireCount)
+    {
+        for (int i = 0; i < fireCount; i++)
+        {
+            StartCoroutine(FireMissileCor());
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+    IEnumerator FireMissileCor()
+    {
+        var alert = missileAlertList.Find(a => !a.gameObject.activeSelf);
+
+        if(alert == null)
+        {
+            alert = Instantiate(missileAlertList[0], missileAlertList[0].transform.parent);
+            missileAlertList.Add(alert);
+        }
+
+        alert.gameObject.SetActive(true);
+
+        float delayTimer = 0f;
+
+        while(delayTimer <= 1.5f)
+        {
+            //missileAlertTr.transform.DOMoveY(GameManager.instance.Player.transform.position.y, Time.deltaTime);
+            Vector3 movePos = new Vector3(alert.transform.position.x, GameManager.instance.Player.transform.position.y, 0f);
+            alert.transform.position = Vector3.Lerp(alert.transform.position, movePos, Time.deltaTime * 2f);
+            delayTimer += Time.deltaTime;
+
+            yield return null;
+        }
+
+        alert.gameObject.SetActive(false);
         MissileController missile = PoolManager.Instance.Pop(missilePrefab).GetComponent<MissileController>();
 
-        var firePos = missileFirePosList[Random.Range(0, missileFirePosList.Count)];
-        missile.transform.position = firePos.transform.position;
-        missile.transform.rotation = firePos.transform.rotation;
+        missile.transform.position = alert.transform.position - alert.transform.right;
+        missile.transform.rotation = alert.transform.rotation;
         missile.gameObject.SetActive(true);
         missile.Init();
-        missile.Fire(firePos.transform.right);
+        missile.Fire(alert.transform.right);
     }
 }
--- Assets/01.Scripts/Obstacles/ObstacleSpawner.cs
+++ Assets/01.Scripts/Obstacles/ObstacleSpawner.cs
@@ -14,6 +14,10 @@ public class ObstacleSpawner : MonoBehaviour
     [PropertyRange(0, 1f)]
     public float missileSpawnRate;
 
+    [FoldoutGroup("장애물 소환 세팅")]
+    [LabelText("미사일 최대 연속발사 수")]
+    public int maxMissileCount = 3;
+    
     [Title("전기장")]
     [FoldoutGroup("장애물 소환 세팅")]
     [LabelText("전기장 소환 확률")]
@@ -31,7 +35,13 @@ public class ObstacleSpawner : MonoBehaviour
 
         if(randomValue <= missileSpawnRate)
         {
-            missileST.FireMissile();
+            int fireCount;
+            if (Time.time > 60f)
+                fireCount = Random.Range(1, maxMissileCount + 1);
+            else
+                fireCount = 1;
+
+            missileST.FireMissile(fireCount);
         }
 
         randomValue = Random.value;
